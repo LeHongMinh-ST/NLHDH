e55b49cbee1c5f4dc2ad4f9bcc2e20072bc8892f
Dam Duc Chien <chiendam1908@gmail.com>
2020-03-26 17:05:06 +0700

    Update giai thuat FCFS

--- Các bài code/FCFS.cpp
+++ Các bài code/FCFS.cpp
@@ -3,13 +3,11 @@
 
 using namespace std;
 
-void doiCho(int &a, int &b);
-
 int main()
 {
 	//khai bao bien thoi gian BurstTime và WaitingTime
 	int bt[30], wt[30] ;
-	int n, i, j, tb =0, k;
+	int n, i, j, tb =0, k, tg = 0;
 	int p[30];
 	
 	cout<<"Nhap vao so tien trinh: ";
@@ -26,26 +24,14 @@ int main()
 	cout<<"\nProcess\t    Burst Time    \tWaiting Time";
 	for(i=0;i<n;i++)
     {
-    	tb += bt[i]; 
-    	wt[i] = tb - bt[i];
+    	tg += bt[i]; 
+    	wt[i] = tg - bt[i];
+		tb += wt[i];
         cout<<"\nP["<<p[i]<<"]\t\t  "<<bt[i]<<"\t\t    "<<wt[i];
     }
 	
-	tb = 0;
-	cout<<"\n\nThoi gian cho tb = ";
-	for(i = 0;i<n;i++)
-	{
-		tb += wt[i];
-	}
-	cout<<tb/n;
+	cout<<"\n\nThoi gian cho tb = "<<tb/n;
 	
 	cout<<endl;
 	return 0;
-}
-//Dinh nghia ham
-void doiCho(int &a, int &b)
-{
-	int tg = a;
-	a = b;
-	b = tg;
 }
